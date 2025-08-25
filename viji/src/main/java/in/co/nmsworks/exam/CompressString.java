b239e39fa6fd8695355a0b16ec2bb88453b983e6
vijayalakshmi <vijayalakshmir@nmsworks.co.in>
2025-08-25 12:47:08 +0530

    Shipping Inteface Program

--- viji/src/main/java/in/co/nmsworks/exam/CompressString.java
+++ viji/src/main/java/in/co/nmsworks/exam/CompressString.java
@@ -1,4 +1,8 @@
 package in.co.nmsworks.exam;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 /* 2. Compress String [5 Marks]
 Write a method private String compressString(String input) that takes a string and
 compresses it by counting consecutive occurrences of each character. The compressed version should
@@ -14,13 +18,14 @@ public class CompressString {
     }
     private static String compressString(String input){
        String output = "";
+       Set<String> set = new LinkedHashSet<>();
 
         for(int i =0 ; i<input.length () ; i++){
             char ch = input.charAt(i);
             if(input.indexOf(ch)==-1){
                 int count = 0;
                 count++;
-                output = output +"";
+                output = output +ch;
             }
 
         }
