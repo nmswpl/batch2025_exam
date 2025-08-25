5919f042f93acdd8ef83bdf68f652ff0c3350c9e
logeswarin <logeswari@nmsworks.co.in>
2025-08-25 12:20:49 +0530

    4.Login Credential
    done.

--- logeswari/src/main/java/in/co/nmsworks/UserDeatilsGet.java
+++ logeswari/src/main/java/in/co/nmsworks/UserDeatilsGet.java
@@ -9,6 +9,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class UserDeatilsGet {
     public static void main(String[] args) {
@@ -67,6 +68,41 @@ public class UserDeatilsGet {
         }
         return readDate();
     }
+
+    public Set<String>getActiveFemalename(List<UserDeatilsGet> deatilsGets){
+//        String sql="INSERT INTO user_details Values(?,?,?,?,?,?)";
+        String sql="Select * from user_details where gender=female";
+        List<Integer> deatilGets=new ArrayList<>();
+
+        try(Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/training","nms-training","nms-training");
+            BufferedReader br= new BufferedReader(new FileReader("/home/nms-training/git/batch2025_exam/logeswari/src/main/resources/user_details.sql")))
+        {
+            try(PreparedStatement preparedStatement= con.prepareStatement(sql);
+                ResultSet rs= preparedStatement.getResultSet()) {
+
+                String line = "";
+                while (br.readLine()!=null){
+                    String[] word=line.split(",");
+                    int id=rs.getInt(1);
+                    String name=rs.getString(2);
+                    String gender=rs.getString(3);
+                    String userName=rs.getString(4);
+                    String password=rs.getString(5);
+                    boolean acStatus=rs.getBoolean(6);
+                    preparedStatement.executeQuery();
+                    deatilGets.add(readDate().size());
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return getActiveFemalename(deatilsGets);
+    }
+
 
 
 }
