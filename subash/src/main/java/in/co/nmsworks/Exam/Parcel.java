21c5760a3cb40dc796f9c7833a80fab40a980387
Subashnms <subashm@nmsworks.co.in>
2025-08-25 12:38:41 +0530

    Final Exam

---
+++ subash/src/main/java/in/co/nmsworks/Exam/Parcel.java
@@ -0,0 +1,7 @@
+package in.co.nmsworks.Exam;
+
+public class Parcel
+{
+    private int weight;
+    private int distance;
+}
---
+++ subash/src/main/java/in/co/nmsworks/Exam/ShippingCostCalculator.java
@@ -0,0 +1,36 @@
+package in.co.nmsworks.Exam;
+
+public class ShippingCostCalculator
+{
+
+    public static void main(String[] args) {
+
+
+        ShippingCostCalculator scc =new ShippingCostCalculator();
+        ShippingMethod as = new AirShipping();
+        ShippingMethod ls = new LandShipping();
+        ShippingMethod ss = new SeaShipping();
+
+        Parcel pp = new Parcel();
+
+
+        scc.calculateTotalCost(as,pp);
+    }
+
+
+
+
+    public void calculateTotalCost(ShippingMethod shippingMethod, Parcel parcel)
+    {
+
+
+
+
+
+
+    }
+
+
+
+
+}
