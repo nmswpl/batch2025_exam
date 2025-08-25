eb4d34537eaf4fda6428932ff39c7a9bb0e8ace9
Mohamed Asik <mohamedasikm@nmsworks.co.in>
2025-08-25 12:49:19 +0530

    Exam Questions
    Implemented Question 3

--- asik/src/main/java/in/co/nmsworks/test/SaleInfo.java
+++ asik/src/main/java/in/co/nmsworks/test/SaleInfo.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class SaleInfo
 {
     private List<SalesData> salesDataList = new ArrayList<>();
-    Map<String, Double[]> avgPrice = new HashMap<>();
+    private Map<String, Double[]> avgPrice = new HashMap<>();
 
     public static void main(String[] args)
     {
