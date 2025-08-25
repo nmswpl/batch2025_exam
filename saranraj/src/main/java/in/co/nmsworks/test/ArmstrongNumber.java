144ff3c29f53ed001f042265cbbca6d395ae5320
saranraji <saranraji@nmsworks.co.in>
2025-08-25 10:32:36 +0530

    Exam

---
+++ saranraj/src/main/java/in/co/nmsworks/test/ArmstrongNumber.java
@@ -0,0 +1,36 @@
+package in.co.nmsworks.test;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        ArmstrongNumber am = new ArmstrongNumber();
+        am.findArmstrongNumber();
+    }
+
+    private void findArmstrongNumber() {
+        int n = 17;
+        for (int i = 10; i < n; i++) {
+            if (isArmStrongNumber(i)) {
+                System.out.println("The ArmStrong Number :" + i);
+            } else {
+                System.out.println("Not an ArmStrong Number");
+            }
+        }
+    }
+
+    private boolean isArmStrongNumber(int num) {
+
+
+        int original = num;
+
+        int armStrongNumber = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            armStrongNumber += Math.pow(digit, 3);
+            num /= 10;
+        }
+        if (original != armStrongNumber) {
+            return false;
+        }
+        return true;
+    }
+}
