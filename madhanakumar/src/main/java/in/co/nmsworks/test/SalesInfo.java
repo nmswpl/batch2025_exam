8e762440f20724a8d57f6d622aa02141687a7d93
Madhanakumar <madhanakumart@nmsworks.co.in>
2025-08-25 12:36:25 +0530

    Committing the Third test problem
    CompressString problem committed successfully

--- madhanakumar/src/main/java/in/co/nmsworks/test/SalesInfo.java
+++ madhanakumar/src/main/java/in/co/nmsworks/test/SalesInfo.java
@@ -16,24 +16,25 @@ public class SalesInfo {
     private void averagePrice(String filename) {
         try(BufferedReader bw = new BufferedReader(new FileReader(filename));
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/training","nms-training","nms-training");
-            PreparedStatement ps = con.prepareStatement("INSERT TABLE SaleInfo INTO Values (?,?,?,?,?,?)");
-            PreparedStatement ps1 = con.prepareStatement("Select DISTINCT category, AVG(price) from SalesInfo GROUP BY category;")) {
+            PreparedStatement ps = con.prepareStatement("INSERT INTO SalesInfo Values (?,?,?,?,?,?)");
+            PreparedStatement ps1 = con.prepareStatement("Select DISTINCT category, AVG(price) as Average_price from SalesInfo GROUP BY category;")) {
             bw.readLine();
             String lines;
-            SalesPersistor salesPersistor = null;
             while ((lines = bw.readLine()) != null) {
                 String[] strings = lines.split(",");
-                salesPersistor = new SalesPersistor(strings[0], strings[1], strings[2], Double.parseDouble(strings[3]), Double.parseDouble(strings[4]), strings[5]);
+                SalesPersistor salesPersistor = new SalesPersistor(strings[0], strings[1], strings[2], Double.parseDouble(strings[3]), Double.parseDouble(strings[4]), strings[5]);
+                ps.setString(1,salesPersistor.getSales_id());
+                ps.setString(2,salesPersistor.getProduct_name());
+                ps.setString(3,salesPersistor.getCategory());
+                ps.setDouble(6,salesPersistor.getPrice());
+                ps.setDouble(4,salesPersistor.getItems_sold());
+                ps.setString(5,salesPersistor.getSale_date());
+//                ps.executeUpdate();
             }
-            ps.setString(1,salesPersistor.getSales_id());
-            ps.setString(2,salesPersistor.getProduct_name());
-            ps.setString(3,salesPersistor.getCategory());
-            ps.setDouble(4,salesPersistor.getPrice());
-            ps.setDouble(5,salesPersistor.getItems_sold());
-            ps.setString(6,salesPersistor.getSale_date());
-            ps.executeUpdate();
+            ps1.execute();
+            System.out.println(ps1);
         } catch (Exception e){
             e.printStackTrace();
-        } 
+        }
     }
 }
