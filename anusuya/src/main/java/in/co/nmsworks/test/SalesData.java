f4e9556dead56ac25f38d42ad35a77ce3ee27dcc
AnusuyaDevi <anusuyadevir@nmsworks.co.in>
2025-08-25 11:42:57 +0530

    SalesData Implementations
    Implementing two methods having one is prints the average price for each category and another one is print sales info based on month which the products having sold more than others.

---
+++ anusuya/src/main/java/in/co/nmsworks/test/SalesData.java
@@ -0,0 +1,82 @@
+package in.co.nmsworks.test;
+
+public class SalesData {
+
+    private String salesId;
+    private String productName;
+    private String category;
+    private double price;
+    private int noOfItemSold;
+    private String salesDate;
+
+    SalesData(){}
+
+    public SalesData (String salesId, String productName, String category, double price, int noOfItemSold, String salesDate){
+        this.salesId = salesId;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.noOfItemSold = noOfItemSold;
+        this.salesDate = salesDate;
+    }
+
+    public void setSalesId(String salesId) {
+        this.salesId = salesId;
+    }
+
+    public String getSalesId() {
+        return salesId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setNoOfItemSold(int noOfItemSold) {
+        this.noOfItemSold = noOfItemSold;
+    }
+
+    public int getNoOfItemSold() {
+        return noOfItemSold;
+    }
+
+    public void setSalesDate(String salesDate) {
+        this.salesDate = salesDate;
+    }
+
+    public String getSalesDate() {
+        return salesDate;
+    }
+
+    @Override
+    public String toString() {
+        return "SalesData{" +
+                "salesId=" + salesId +
+                ", productName='" + productName + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                ", noOfItemSold=" + noOfItemSold +
+                ", salesDate='" + salesDate + '\'' +
+                '}';
+    }
+}
