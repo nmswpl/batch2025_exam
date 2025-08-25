b2e36136a23ed42d34800996b85b472f035117f9
Arsath457 <arsathahameda@nmsworks.co.in>
2025-08-25 12:50:37 +0530

    Problem 1 and 5

--- arsath/src/main/java/in/co/nmsworks/exam/ArmstrongNumber.java
+++ arsath/src/main/java/in/co/nmsworks/exam/ArmstrongNumber.java
@@ -8,11 +8,18 @@ public class ArmstrongNumber {
 
     public void printArmstrongNumber(int n){
         int sum = 0;
+        int sumOfCubes = 0;
         for(int i = 10; i<=100; i++){
-            int lastDigit = n / 10;
-            sum += (int)Math.pow(lastDigit,3);
-            System.out.println(sum);
+            int lastDigit = n % 10;
+            sum += lastDigit;
+            sumOfCubes += Math.pow(lastDigit,3);
             n/=10;
+        }
+        if(sum==sumOfCubes){
+            System.out.println(n + " is a Armstrong Number");
+        }
+        else {
+            System.out.println("Not an ARmsrong Nmber");
         }
     }
 }
