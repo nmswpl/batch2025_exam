a25909a6af5035b438034317397fde108fc8cdbd
Madhanakumar <madhanakumart@nmsworks.co.in>
2025-08-25 11:15:19 +0530

    Committing the Second test problem
    CompressString problem committed successfully

---
+++ madhanakumar/src/main/java/in/co/nmsworks/test/CompressString.java
@@ -0,0 +1,30 @@
+package in.co.nmsworks.test;
+
+import java.util.Objects;
+
+public class CompressString {
+    public static void main(String[] args) {
+        CompressString cs = new CompressString();
+        cs.compressString("aaabbca");
+    }
+
+    private String compressString(String input) {
+        String output = "";
+        String [] strings = input.split("");
+        for (int i = 0; i < strings.length ; i++){
+            int occur = 0;
+            for (int j = 1; i < strings.length; i++) {
+                if (strings[i].equals(strings[j-1])){
+                    occur += 1;
+                }
+                output += occur+strings[i];
+            }
+
+            occur = 0;
+
+            System.out.println(output);
+        }
+
+        return "";
+    }
+}
