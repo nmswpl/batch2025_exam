a0a8f097535f8fe304fbbe73a0ddce0a13808761
Madhanakumar <madhanakumart@nmsworks.co.in>
2025-08-25 10:43:45 +0530

    Committing the first test problem
    ArmStrong Number problem committed successfully

---
+++ madhanakumar/src/main/java/in/co/nmsworks/test/ArmstrongNumber.java
@@ -0,0 +1,37 @@
+package in.co.nmsworks.test;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        ArmstrongNumber armstrongNumber = new ArmstrongNumber();
+        for (int i = 1; i <= 999; i++) {
+            armstrongNumber.isArmstrongNumber(i);
+        }
+    }
+
+    private boolean isArmstrongNumber(int i) {
+        int res = i;
+        int temp = i;
+        double num = 0;
+        int sum = 0;
+        double count = 0;
+        while (temp > 0){
+            count++;
+            temp = temp /10;
+        }
+
+        while (i > 0){
+            num = i % 10;
+            sum = (int) (sum + Math.pow(num,count));
+            i = i / 10;
+        }
+        System.out.println(sum);
+        if(res == sum){
+            System.out.println(res+" is an Armstrong Number..");
+            return true;
+        }
+        else {
+            System.out.println(res + " is not an Armstrong Number..");
+            return false;
+        }
+    }
+}
