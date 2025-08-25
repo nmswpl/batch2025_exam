b9b4f42e4ecda07f50ca82124b1218eef300eacf
Gajendran <gajendrans@nmsworks.co.in>
2025-08-25 12:49:23 +0530

    commiting
    final commit

--- gajendran/src/main/java/in/co/nmsworks/training_exam_2025/ShippingCostCalculator.java
+++ gajendran/src/main/java/in/co/nmsworks/training_exam_2025/ShippingCostCalculator.java
@@ -7,8 +7,14 @@ public class ShippingCostCalculator
         return 0;
     }
 
-    public void generateQuotation(Parcel parcel){
-
+    public void generateQuotation(Parcel parcel)
+    {
+        ShippingMethod air = new AirShipping();
+        System.out.println("Air Shipping: "+air.additionalDistanceCost(parcel.getDistance())+air.additionalWeightCost(parcel.getWeight()));
+        ShippingMethod alandir = new LandShipping();
+        System.out.println("Air Shipping: "+alandir.additionalDistanceCost(parcel.getDistance())+long.additionalWeightCost(parcel.getWeight()));
+        ShippingMethod sea = new SeaShipping();
+        System.out.println("Air Shipping: "+sea.additionalDistanceCost(parcel.getDistance())+sea.additionalWeightCost(parcel.getWeight()));
     }
 
 }
