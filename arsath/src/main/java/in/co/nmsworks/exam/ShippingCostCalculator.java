eb6c97e5d1f18c666f1a1dfd1669279b27fe11f9
Arsath457 <arsathahameda@nmsworks.co.in>
2025-08-25 12:42:21 +0530

    Problem 1 and 5

---
+++ arsath/src/main/java/in/co/nmsworks/exam/ShippingCostCalculator.java
@@ -0,0 +1,15 @@
+package in.co.nmsworks.exam;
+
+public class ShippingCostCalculator {
+    public static void main(String[] args) {
+
+    }
+
+    public void calculateTotalCost(ShippingMethod shippingMethod, Parcel parcel){
+
+    }
+
+    public void generateQuotation(Parcel parcel){
+
+    }
+}
