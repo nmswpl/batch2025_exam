3f7a678741113bb4cfea1375c0553a59e9719266
Arsath457 <arsathahameda@nmsworks.co.in>
2025-08-25 12:47:21 +0530

    Problem 1 and 5

--- arsath/src/main/java/in/co/nmsworks/exam/ArmstrongNumber.java
+++ arsath/src/main/java/in/co/nmsworks/exam/ArmstrongNumber.java
@@ -7,15 +7,12 @@ public class ArmstrongNumber {
     }
 
     public void printArmstrongNumber(int n){
-//        int sum = 0;
-//       int digit = n / 10;
-//       int sum += digit;
-//       int n1 = (int)Math.pow(digit,3);
-//        n/=10;
-//
-//
-//        System.out.println(n1);
-
-
+        int sum = 0;
+        for(int i = 10; i<=100; i++){
+            int lastDigit = n / 10;
+            sum += (int)Math.pow(lastDigit,3);
+            System.out.println(sum);
+            n/=10;
+        }
     }
 }
