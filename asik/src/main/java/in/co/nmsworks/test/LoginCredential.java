7c5ae04ffeb9d87089ad45694559ecde35c09229
Mohamed Asik <mohamedasikm@nmsworks.co.in>
2025-08-25 12:48:53 +0530

    Exam Questions
    Implemented Question 3

--- asik/src/main/java/in/co/nmsworks/test/LoginCredential.java
+++ asik/src/main/java/in/co/nmsworks/test/LoginCredential.java
@@ -1,11 +1,6 @@
 package in.co.nmsworks.test;
 
-import com.mysql.cj.protocol.Resultset;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Scanner;
 
 public class LoginCredential
@@ -16,27 +11,29 @@ public class LoginCredential
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter UserName : ");
-        String userName = sc.nextLine();
+        String username = sc.nextLine();
         System.out.println("Enter Password :");
         String password = sc.nextLine();
 
-        boolean isValid = loginCredential.checkUserNameAndPassword(userName, password);
+        boolean isValid = loginCredential.checkUserNameAndPassword(username, password);
 
         System.out.println(isValid ? "Valid Credentials" : "Invalid Credentials");
     }
 
-    private boolean checkUserNameAndPassword(String userName, String password)
+    private boolean checkUserNameAndPassword(String username, String password)
     {
         try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/training", "nms-training", "nms-training");
              PreparedStatement pstmt = con.prepareStatement("select * from user_details where username = ? and password = ?"))
         {
-            pstmt.setString(1, userName);
+            pstmt.setString(1, username);
             pstmt.setString(2, password);
-            Resultset rs = (Resultset) pstmt.executeQuery();
-            while (rs.getRows().hasNext())
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next())
             {
                 return true;
             }
+
         }
         catch (SQLException se)
         {
