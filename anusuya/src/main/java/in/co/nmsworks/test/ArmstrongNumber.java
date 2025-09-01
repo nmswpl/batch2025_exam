41f04af5d9d14d75243ef11ac38258d7542b2d84
AnusuyaDevi <anusuyadevir@nmsworks.co.in>
2025-09-01 14:27:04 +0530

    ArmsStrong Number.
    Find and print first seven armstrong numbers and handling edge cases.

--- anusuya/src/main/java/in/co/nmsworks/test/ArmstrongNumber.java
+++ anusuya/src/main/java/in/co/nmsworks/test/ArmstrongNumber.java
@@ -1,38 +1,50 @@
 package in.co.nmsworks.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArmstrongNumber {
 
     public static void main(String[] args) {
 
         ArmstrongNumber an = new ArmstrongNumber();
-        an.printFirstSevenArmstrongNumber();
-    }
 
-    private void printFirstSevenArmstrongNumber() {
-
-        System.out.println("FIRST SEVEN ARMSTRONG NUMBERS : ");
-        int count = 0;
-        for (int i = 10; i <= 10000; i++){
-            int num = i;
-            int number = i;
-            int no_of_digits = 0;
-            int sum = 0;
-            if(count == 7){
-                break;
-            }
-            while (number > 0) {
-                number /= 10;
-                no_of_digits++;
-            }
-            while (num > 0){
-                int rem = num % 10;
-                sum += Math.pow(rem,no_of_digits);
-                num /= 10;
-            }
-            if (sum == i) {
-                System.out.print(i + " ");
+        int count = 1;
+        int number = 10;
+        List<Integer> armstrongNumberList = new ArrayList<>();
+        List<Integer> nonArmstrongNumberList = new ArrayList<>();
+        while (count <= 7){
+            if (an.isArmstrongNumber(number)){
+                armstrongNumberList.add(number);
                 count++;
             }
+            else{
+                nonArmstrongNumberList.add(number);
+            }
+            number++;
         }
+        System.out.println("ARMSTRONG NUMBERS ARE \n" + armstrongNumberList);
+        System.out.println("NON ARMSTRONG NUMBERS ARE \n" + nonArmstrongNumberList);
+    }
+
+    private boolean isArmstrongNumber(int i) {
+
+        int num = i;
+        int number = i;
+        int no_of_digits = 0;
+        int sum = 0;
+        while (number > 0) {
+            number /= 10;
+            no_of_digits++;
+        }
+        while (num > 0) {
+            int rem = num % 10;
+            sum += Math.pow(rem, no_of_digits);
+            num /= 10;
+        }
+        if (sum == i) {
+            return true;
+        }
+        return false;
     }
 }
