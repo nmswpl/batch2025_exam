5e7152a51d0a1ef217cef715bc006c68755a6990
Arsath457 <arsathahameda@nmsworks.co.in>
2025-09-01 17:33:51 +0530

    Exam Questions
    Question 2 Completed

--- arsath/src/main/java/in/co/nmsworks/exam2/Matrix.java
+++ arsath/src/main/java/in/co/nmsworks/exam2/Matrix.java
@@ -1,24 +1,53 @@
 package in.co.nmsworks.exam2;
 
-import java.util.Arrays;
-
 public class Matrix {
-    public static void main(String[] args) {
-        int[][] matrix = { {1,2,3}, {4,5,6}, {7,8,9} };
-        int[][] rotatedMatrix = new int[3][3];
 
-        for (int i = 0; i <= matrix.length-1; i++) {
-            for (int j =0; j <= matrix.length -1 ; j++) {
-                rotatedMatrix[i][j] = matrix[matrix.length-1][i];
+    public static void rotateMatrixClockwise(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
 
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
 
-        for (int i = 0; i < rotatedMatrix.length; i++) {
-            for (int j = 0; j < rotatedMatrix.length ; j++) {
-                System.out.print(rotatedMatrix[i][j]);
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        rotateMatrixClockwise(matrix);
+
+        System.out.println("Rotated Matrix:");
+        printMatrix(matrix);
     }
 }
