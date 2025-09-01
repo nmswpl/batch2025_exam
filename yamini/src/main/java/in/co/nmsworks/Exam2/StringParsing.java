12e3e5ffd30ba3ad855233b2bcc32409e7ce777e
Yamini <yamini@nmsworks.co.in>
2025-09-01 16:01:19 +0530

    Exam problems
    StringParsing program

---
+++ yamini/src/main/java/in/co/nmsworks/Exam2/StringParsing.java
@@ -0,0 +1,14 @@
+package in.co.nmsworks.Exam2;
+
+public class StringParsing {
+    public static void main(String[] args) {
+        String input = "{ \"name\": \"John\", \"age\": 30, \"department\": \"cse\"}";
+        String[] arr = input.split("\\W+".trim());
+        for(int i=1;i<arr.length-1;i++){
+         System.out.println(arr[i].trim()+" is "+arr[i+1].trim());
+         i++;
+
+        }
+
+    }
+}
