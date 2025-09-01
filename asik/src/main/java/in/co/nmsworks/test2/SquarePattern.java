e4b05fffc9ff67ac169f570633d3cdc60302b862
Mohamed Asik <mohamedasikm@nmsworks.co.in>
2025-09-01 15:33:27 +0530

    Exam Questions
    Question 4 Square Pattern

--- asik/src/main/java/in/co/nmsworks/test2/SquarePattern.java
+++ asik/src/main/java/in/co/nmsworks/test2/SquarePattern.java
@@ -7,6 +7,7 @@ public class SquarePattern
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter the Side Size : ");
         int side = sc.nextInt();
         sc.nextLine();
