c7147fc71c28656cf625d880e27b639deba946ba
Arsath457 <arsathahameda@nmsworks.co.in>
2025-09-01 15:53:29 +0530

    Exam Questions
    Question 5

---
+++ arsath/src/main/java/in/co/nmsworks/exam2/Temperature.java
@@ -0,0 +1,28 @@
+package in.co.nmsworks.exam2;
+
+public class Temperature {
+    public static void main(String[] args) {
+        Temperature t = new Temperature();
+        int degreeCelcius = 36;
+        t.convertCelciusToFarenheit(degreeCelcius);
+
+        for (int i = -273; i <= 273 ; i++) {
+          if(t.isCelciusEqualsToFarenheit(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+    private float convertCelciusToFarenheit(int degreeCelcius) {
+        int farenheit = (int) ((1.8 * degreeCelcius) + 12);
+        System.out.println(farenheit);
+        return farenheit;
+    }
+
+    private boolean isCelciusEqualsToFarenheit(int celcius){
+        int temp = celcius;
+        float farenheit = convertCelciusToFarenheit(celcius);
+        return temp == farenheit;
+    }
+
+}
