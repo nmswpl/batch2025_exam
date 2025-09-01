38f6ee5ef4b50f2bf03872e697553ab8f335d077
Harish <harishg@nmsworks.co.in>
2025-09-01 14:27:17 +0530

    Changes in the test files
    Some changes are happened

--- harish/src/main/java/in/co/nmsworks/test/ArmstrongNumber.java
+++ harish/src/main/java/in/co/nmsworks/test/ArmstrongNumber.java
@@ -5,33 +5,27 @@ public class ArmstrongNumber {
         int num = 10;
         int j = 0;
         ArmstrongNumber an = new ArmstrongNumber();
-       while (j < 7)
-       {
-           boolean isArmstrong = an.isArmstrongNumber(num);
-           if (isArmstrong){
-               System.out.println("This is an armstrong number :" + num );
-               j++;
-           }
-           num ++;
-       }
+        while (j < 7) {
+            boolean isArmstrong = an.isArmstrongNumber(num);
+            if (isArmstrong) {
+                System.out.println("This is an armstrong number :" + num);
+                j++;
+            }
+            num++;
+        }
     }
 
     private boolean isArmstrongNumber(int num) {
 
-           int number = num;
-           double digit;
-           double sum = 0;
-           while (number > 0){
-               digit = number % 10;
-               sum = sum + Math.pow(digit,3);
-               number = number / 10;
+        int number = num;
+        double digit;
+        double sum = 0;
+        while (number > 0) {
+            digit = number % 10;
+            sum = sum + Math.pow(digit, 3);
+            number = number / 10;
 
-           }
-           if(sum == num){
-               return true;
-           }
-           else{
-               return false;
-           }
+        }
+        return sum == num;
     }
 }
--- harish/src/main/java/in/co/nmsworks/test/ShippingCostCalculator.java
+++ harish/src/main/java/in/co/nmsworks/test/ShippingCostCalculator.java
@@ -8,13 +8,33 @@ public class ShippingCostCalculator {
         Parcel parcel = new Parcel(200, 455);
         ShippingMethod sm = new Airshipping();
         double totalCost = calculateTotalCost(sm, parcel);
-        double  quatt = generateQuatation(parcel);
+        System.out.println(totalCost);
+        generateQuatation(parcel);
     }
 
 
 
     private static double calculateTotalCost(ShippingMethod sm, Parcel parcel) {
-       if (sm.getClass().equals("AirShipping")){
+
+        double weight = parcel.getWeight();
+        double distance = parcel.getDistance();
+        double totalCost;
+        if (weight > sm.getBaseWeight()) {
+            weight = weight - sm.getBaseWeight();
+            totalCost = sm.getPrice() + sm.additionalWeightCost(weight);
+        } else {
+            totalCost = sm.getPrice();
+        }
+        if (distance > sm.getBaseDistance()) {
+            distance = distance - sm.getBaseDistance();
+            totalCost = totalCost + sm.additionalDistanceCost(distance);
+        }
+
+        if (sm instanceof Airshipping)
+            totalCost = totalCost * 2.5;
+
+        return totalCost;
+       /*if (sm.getClass().equals("AirShipping")){
            double weight = parcel.getWeight();
            double distance = parcel.getDistance();
            double totalCost;
@@ -46,25 +66,17 @@ public class ShippingCostCalculator {
                totalCost = totalCost + sm.additionalDistanceCost(distance);
            }
            return totalCost;
-       }
+       }*/
     }
-    private static double generateQuatation(Parcel parcel) {
-        ShippingMethod sm = new Airshipping();
-        double weight = parcel.getWeight();
-        double distance = parcel.getDistance();
-        double totalCost;
-        if (weight > sm.getBaseWeight()) {
-            weight = weight - sm.getBaseWeight();
-            totalCost = sm.getPrice() + sm.additionalWeightCost(weight);
-        } else {
-            totalCost = sm.getPrice();
-        }
-        if (distance > sm.getBaseDistance()) {
-            distance = distance - sm.getBaseDistance();
-            totalCost = totalCost + sm.additionalDistanceCost(distance);
-        }
-        totalCost = totalCost * 2.5;
-        return totalCost;
+
+    private static void  generateQuatation(Parcel parcel) {
+        Airshipping as = new Airshipping();
+        SeaShipping ss = new SeaShipping();
+        LandShipping ls = new LandShipping();
+
+        System.out.println("AS Cost = " + calculateTotalCost(as, parcel));
+        System.out.println("LS Cost = " + calculateTotalCost(ls, parcel));
+        System.out.println("SS Cost = " + calculateTotalCost(ss, parcel));
     }
 }
 
