6d9ba495072a5f228a78f2cf795229e20588a489
vijayalakshmi <vijayalakshmir@nmsworks.co.in>
2025-09-01 14:52:25 +0530

    question4 program

--- viji/src/main/java/in/co/nmsworks/exam2/Question4.java
+++ viji/src/main/java/in/co/nmsworks/exam2/Question4.java
@@ -2,7 +2,7 @@ package in.co.nmsworks.exam2;
 
 import java.util.Scanner;
 
-public class question4 {
+public class Question4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the length :");
