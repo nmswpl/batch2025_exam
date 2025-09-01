78e8e5f6e705f11d5144e63efa9e833596de31bc
vijayalakshmi <vijayalakshmir@nmsworks.co.in>
2025-09-01 14:51:46 +0530

    question4 program

---
+++ viji/src/main/java/in/co/nmsworks/exam2/question4.java
@@ -0,0 +1,26 @@
+package in.co.nmsworks.exam2;
+
+import java.util.Scanner;
+
+public class question4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the length :");
+        int n = sc.nextInt();
+        lengthOfSquare(n);
+    }
+
+    private static void lengthOfSquare(int n) {
+        for(int i =1 ; i <= n ; i++){
+            for(int  j= 1; j<=n ;j++){
+                if(i==1 || j==1|| i==n || j==n){
+                    System.out.print(i);
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
