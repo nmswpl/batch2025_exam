0cb647c5133ed20b5e9fc789df11c4e7ab887b98
AnusuyaDevi <anusuyadevir@nmsworks.co.in>
2025-09-01 14:27:26 +0530

    String Compression.
    Compressing string and handling edge cases.

--- anusuya/src/main/java/in/co/nmsworks/test/CompressString.java
+++ anusuya/src/main/java/in/co/nmsworks/test/CompressString.java
@@ -6,7 +6,7 @@ public class CompressString {
 
         CompressString cs = new CompressString();
 
-        String input = "aaabbcc";
+        String input = null;
         String result = cs.compressString(input);
 
         System.out.println("Input String before Compression : " + input);
@@ -15,24 +15,36 @@ public class CompressString {
 
     private String compressString(String input){
 
-        StringBuilder resultString = new StringBuilder();
-
-        char[] inputArray = input.toCharArray();
-        int count = 1;
-        int i;
-
-        for (i = 1; i < inputArray.length; i++){
-            if (inputArray[i] == inputArray[i-1]){
-                count++;
-            }
-            else{
-                resultString.append(count);
-                resultString.append(inputArray[i-1]);
-                count = 1;
-            }
+        if (input.isEmpty()){
+            System.out.println("Empty String!!");
         }
-        resultString.append(count);
-        resultString.append(inputArray[i-1]);
-        return resultString.toString();
+
+        if (input == null){
+            System.out.println("CAN'T DO ANY OPERATIONS IN NULL");
+        }
+
+        if (!input.isEmpty()){
+            StringBuilder resultString = new StringBuilder();
+
+            char[] inputArray = input.toCharArray();
+            int count = 1;
+            int i;
+
+
+            for (i = 1; i < inputArray.length; i++){
+                if (inputArray[i] == inputArray[i-1]){
+                    count++;
+                }
+                else{
+                    resultString.append(count);
+                    resultString.append(inputArray[i-1]);
+                    count = 1;
+                }
+            }
+            resultString.append(count);
+            resultString.append(inputArray[i-1]);
+            return resultString.toString();
+        }
+        return null;
     }
 }
