bc140387c64871d675c6362ae8f2fc2fafe46075
Madhanakumar <madhanakumart@nmsworks.co.in>
2025-09-01 15:39:36 +0530

    Committing the Third test problems
    Third test problem committed successfully

---
+++ madhanakumar/src/main/java/in/co/nmsworks/exam2/NumberToWords.java
@@ -0,0 +1,32 @@
+package in.co.nmsworks.exam2;
+
+public class NumberToWords {
+    String[] belowTwenty = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+    public static void main(String[] args) {
+        int[] givenNumbers = {14, 1234, 50005, 99999, 0};
+        for (int num : givenNumbers) {
+            System.out.println(num + " => " + convert(num));
+        }
+    }
+    public static String convert(int num) {
+        if (num == 0)
+            return "Zero";
+        NumberToWords numberToWords = new NumberToWords();
+        return numberToWords.printNumberToWords(num).trim();
+    }
+    private String printNumberToWords(int num) {
+        String result = "";
+        if (num < 20) {
+            result = belowTwenty[num];
+        } else if (num < 100) {
+            result = tens[num / 10] + " " + belowTwenty[num % 10];
+        } else if (num < 1000) {
+            result = belowTwenty[num / 100] + " Hundred " + printNumberToWords(num % 100);
+        } else if (num < 100000) {
+            result = printNumberToWords(num / 1000) + " Thousand " + printNumberToWords(num % 1000);
+        }
+        return result.trim();
+    }
+}
